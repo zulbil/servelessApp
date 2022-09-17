54ed94cb291bfb6881426ccbebf0f0d7dcb68862
Zulbil <alexkhang25@yahoo.fr>
2022-09-17 17:14:35 -0500

    update index.ts

--- src/functions/images/index.ts
+++ src/functions/images/index.ts
@@ -1,5 +1,18 @@
 import { handlerPath } from '@libs/handler-resolver';
 
+export const getImage = {
+  handler: `${handlerPath(__dirname)}/handler.getImage`,
+  events: [
+    {
+      http: {
+        method: 'get',
+        path: 'images/{id}',
+        cors: true
+      }
+    }
+  ]
+};
+
 export const getImagesByGroup = {
   handler: `${handlerPath(__dirname)}/handler.getImagesByGroup`,
   events: [
@@ -10,5 +23,5 @@ export const getImagesByGroup = {
         cors: true
       }
     }
-  ],
+  ]
 };
